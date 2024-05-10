0caf20c0ee5e77e0ea007d3f746b66b81f6767eb
Rafael de Jesus Carvalho <141766102+rafaeldjcarvalho@users.noreply.github.com>
2024-05-10 15:17:17 -0300

    exercicio01

---
+++ exercicios/aula12/ex011.js
@@ -0,0 +1,21 @@
+var idade = 68
+console.log(`Você tem ${idade} anos.`)
+if (idade < 16) {
+    console.log('Não vota')
+} else if (idade < 18 || idade > 67){
+    console.log('Voto opcional')
+} else {
+    console.log('Voto obrigatório')
+}
+
+/*
+if (idade < 16) {
+    console.log('Não vota')
+} else {
+    if(idade < 18) {
+        console.log('Voto opcional')
+    } else {
+        console.log('Vota')
+    }
+}
+*/
---
+++ exercicios/aula12_ex/ex014/script.js
@@ -0,0 +1,17 @@
+function carregar() {
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    msg.innerHTML = `Agora são ${hora} horas.`
+    if (hora >= 0 && hora < 12){
+        img.src = 'foto-manha.png'
+        document.body.style.background = 'yellow'
+    } else if (hora >= 12 && hora <= 18) {
+        img.src = 'foto-tarde.png'
+        document.body.style.background = 'orange'
+    } else {
+        img.src = 'foto-noite.png'
+        document.body.style.background = 'darkgray'
+    }
+}
