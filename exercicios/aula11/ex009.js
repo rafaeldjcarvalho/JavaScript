313cc4d9366331dbb4b6753492ca16b568c8bf20
Rafael de Jesus Carvalho <141766102+rafaeldjcarvalho@users.noreply.github.com>
2024-05-10 11:05:14 -0300

    javascript exercicios

---
+++ exercicios/aula11/ex009.js
@@ -0,0 +1,14 @@
+var pais = 'Brasil'
+console.log(`Vivendo em ${pais}`)
+if (pais == 'Brasil') {
+    console.log('Você é brasileiro')
+} else {
+    console.log('Você é estrangeiro')
+}
+/*
+if (pais != 'Brasil') {
+    console.log('Você é estrangeiro')
+} else {
+    console.log('Você é brasileiro')
+}
+*/
