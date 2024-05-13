f5819f58c523a033b4477d51f50d774ca9e35290
Rafael de Jesus Carvalho <141766102+rafaeldjcarvalho@users.noreply.github.com>
2024-05-13 17:49:25 -0300

    funcoes part 2

---
+++ exercicios/aula16/ambiente04.js
@@ -0,0 +1,22 @@
+/*
+function fatorial(n) {
+    let fat = 1
+    for(let c = n; c > 1; c--) {
+        fat *= c
+    }
+    return fat
+}
+
+console.log(fatorial(3))
+*/
+// recursao
+
+function fatorial(n) {
+    if (n == 1) {
+        return 1
+    } else {
+        return n * fatorial(n-1)
+    }
+}
+
+console.log(fatorial(4))
