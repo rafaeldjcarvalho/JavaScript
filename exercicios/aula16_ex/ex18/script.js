419fd988ede1ecf71a584984ef7bb72d2c03677f
Rafael de Jesus Carvalho <141766102+rafaeldjcarvalho@users.noreply.github.com>
2024-05-15 08:53:44 -0300

    update ex18

--- exercicios/aula16_ex/ex18/script.js
+++ exercicios/aula16_ex/ex18/script.js
@@ -1,8 +1,4 @@
 let num = []
-let cont = 0
-let maior = 0
-let menor = 0
-let media = 0
 let res = document.getElementById('res')
 let valor = document.getElementById('txtnum')
 let sel = document.querySelector('select#selnum')
@@ -26,12 +22,10 @@ function inLista (v, l) {
 function adicionar() {
     if (isNumero(valor.value) && !inLista(valor.value, num)) {
         // colocar no array
-        num.push(valor.value)
+        num.push(Number(valor.value))
 
         let item = document.createElement('option')
-        cont++
         item.text = `Valor ${valor.value} adicionado.`
-        item.value = `sel${cont}`
         sel.appendChild(item)
 
         // remover dados do finalizar
@@ -39,59 +33,34 @@ function adicionar() {
     } else {
         window.alert('Número inválido ou já encontrado na lista')
     }
+    valor.value = ''
+    valor.focus()
 }
 
 function finalizar() {
     if (num.length == 0) {
         window.alert('Adicione valores antes de finalizar.')
     } else {
-        let p1 = document.createElement('p')
-        let p2 = document.createElement('p')
-        let p3 = document.createElement('p')
-        let p4 = document.createElement('p')
-        let p5 = document.createElement('p')
-
-        p1.innerHTML = `Ao todo, temos ${num.length} números cadastrados.`
-        maiorMenorNum()
-        p2.innerHTML = `O maior valor informado foi ${maior}.`
-        p3.innerHTML = `O menor valor informado foi ${menor}.`
-        let s = somarArray()
-        p4.innerHTML = `Somando todos os valores, temos ${s}.`
-        mediaArray()
-        p5.innerHTML = `A média dos valores digitados é ${media}.`
-
-        res.appendChild(p1)
-        res.appendChild(p2)
-        res.appendChild(p3)
-        res.appendChild(p4)
-        res.appendChild(p5)
-    }
-}
-
-function maiorMenorNum() {
-    maior = num[0]
-    menor = num[0]
-    if (num.length > 1) {
-        for (let c = 0; c < num.length; c++) {
-            if (num[c] > maior) {
-                maior = num[c]
-            } else if (num[c] < menor) {
-                menor = num[c]
+        let tot = num.length
+        let maior = num[0]
+        let menor = num[0]
+        let soma = 0
+        let media = 0
+        for (let pos in num) {
+            soma += num[pos]
+            if (num[pos] > maior) {
+                maior = num[pos]
+            } else if (num[pos] < menor) {
+                menor = num[pos]
             }
         }
+        media = soma / tot
+        res.innerHTML = ''
+        res.innerHTML += `<p>Ao todo, temos ${num.length} números cadastrados.</p>`
+        res.innerHTML += `<p>O maior valor informado foi ${maior}.</p>`
+        res.innerHTML += `<p>O menor valor informado foi ${menor}.</p>`
+        res.innerHTML += `<p>Somando todos os valores, temos ${soma}.</p>`
+        res.innerHTML += `<p>A média dos valores digitados é ${media}.</p>`
+
     }
 }
-
-function somarArray() {
-    let soma = 0
-    for (let c = 0; c < num.length; c++) {
-        soma += num[c]
-    }
-    return soma
-}
-
-function mediaArray() {
-    media = somarArray() / num.length
-}
-
-console.log(num)
